956bca8158006184e2b01e54a2b0406d4baf392c
elkks19 <rafafabiani1909@gmail.com>
2025-10-30 17:58:36 -0400

    cambios a web

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-## GRUPO: Manqha - Pacha
+## GRUPO: Manqha - pacha
 Leyenda asociada: El duende guardian de la noche y el bosque
+
+Pagina web desarrollada en astro.
--- scripts/test.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@onready var color = $"../ColorRect"
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		color.visible = !color.visible
-		
